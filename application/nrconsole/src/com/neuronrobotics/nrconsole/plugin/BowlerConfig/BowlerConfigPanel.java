630f57f4edd155f1a96ec6180c73251a3a98e2d2
DrVulpinus <siganus.vulpinus@live.com>
2014-07-18 17:55:45 -0400

    Added Configuration Data to Config Plugin
    Added elements to interface with the available axes
    Inputs into the text boxes is limited to numbers
    ***Send button not implemented yet***

--- application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/BowlerConfig/BowlerConfigPanel.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/BowlerConfig/BowlerConfigPanel.java
@@ -3,6 +3,7 @@ package com.neuronrobotics.nrconsole.plugin.BowlerConfig;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -19,8 +20,13 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.JList;
 
+import org.jfree.ui.WizardDialog;
 
-public class BowlerConfigPanel extends JPanel{
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class BowlerConfigPanel extends JPanel implements KeyListener{
 
 	/**
 	 * 
@@ -31,16 +37,23 @@ public class BowlerConfigPanel extends JPanel{
 	private List<String> axesNames;
 	private JTextField tfName;
 	private JTextField tfNumAxes;
+	private JList<String> listAxes;
+	private JTextField tfCurrVal;
+	private JTextField tfkP;
+	private JTextField tfkI;
+	private JTextField tfkD;
+	private JTextField tfMin;
+	private JTextField tfMax;
 
 	public BowlerConfigPanel(){
-		setLayout(new MigLayout("", "[grow][grow]", "[][][grow]"));
+		setLayout(new MigLayout("", "[grow][grow][grow]", "[][][grow,center][grow][grow][grow][grow][][][]"));
 		
 		JLabel lblName = new JLabel("Device Name:");
 		add(lblName, "cell 0 0,alignx trailing");
 		
 		tfName = new JTextField();
 		tfName.setEditable(false);
-		add(tfName, "cell 1 0,growx");
+		add(tfName, "cell 1 0 2 1,growx");
 		tfName.setColumns(10);
 		tfName.setText("Printer");
 		
@@ -49,12 +62,86 @@ public class BowlerConfigPanel extends JPanel{
 		
 		tfNumAxes = new JTextField();
 		tfNumAxes.setEditable(false);
-		add(tfNumAxes, "cell 1 1,growx");
+		add(tfNumAxes, "cell 1 1 2 1,growx");
 		tfNumAxes.setColumns(10);
 		tfNumAxes.setText(String.valueOf(printer.getNumberOfLinks()));
 		
-		JList<String> listAxes = new JList<String>();
-		add(listAxes, "cell 0 2,grow");
+		listAxes = new JList<String>();
+		add(listAxes, "cell 0 2 1 6,grow");
+		
+		JLabel lblNewLabel = new JLabel("Current Value");
+		add(lblNewLabel, "cell 1 2,alignx trailing");
+		
+		tfCurrVal = new JTextField();
+		tfCurrVal.addKeyListener(this);		
+		add(tfCurrVal, "cell 2 2,growx");
+		tfCurrVal.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("kP");
+		add(lblNewLabel_1, "cell 1 3,alignx trailing");
+		
+		tfkP = new JTextField();
+		tfkP.addKeyListener(this);
+		add(tfkP, "cell 2 3,growx");
+		tfkP.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("kI");
+		add(lblNewLabel_2, "cell 1 4,alignx trailing");
+		
+		tfkI = new JTextField();
+		tfkI.addKeyListener(this);
+		add(tfkI, "cell 2 4,growx");
+		tfkI.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("kD");
+		add(lblNewLabel_3, "cell 1 5,alignx trailing");
+		
+		tfkD = new JTextField();
+		tfkD.addKeyListener(this);
+		add(tfkD, "cell 2 5,growx");
+		tfkD.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Minimum");
+		add(lblNewLabel_4, "cell 1 6,alignx trailing");
+		
+		tfMin = new JTextField();
+		tfMin.addKeyListener(this);
+		add(tfMin, "cell 2 6,growx");
+		tfMin.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Maximum");
+		add(lblNewLabel_5, "cell 1 7,alignx trailing");
+		
+		tfMax = new JTextField();
+		tfMax.addKeyListener(this);
+		add(tfMax, "cell 2 7,growx");
+		tfMax.setColumns(10);
+		
+		
+		
+		JButton btnSendNewValues = new JButton("Send New Values");
+		btnSendNewValues.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		add(btnSendNewValues, "cell 0 8 3 1,growx");
+		
+		JButton btnGetCurrentValues = new JButton("Get Current Values");
+		btnGetCurrentValues.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				LinkConfiguration selectedLink = printer.getLinkConfiguration(listAxes.getSelectedIndex());
+				tfkP.setText(String.valueOf(selectedLink.getKP()));
+				tfkI.setText(String.valueOf(selectedLink.getKI()));
+				tfkD.setText(String.valueOf(selectedLink.getKD()));
+				tfMin.setText(String.valueOf(selectedLink.getLowerLimit()));
+				tfMax.setText(String.valueOf(selectedLink.getUpperLimit()));
+				
+				
+				
+			}
+		});
+		add(btnGetCurrentValues, "cell 0 9 3 1,growx");
 		for (LinkConfiguration link : printer.getLinkConfigurations()) {
 			axesNames.add(0, link.getName());
 		}
@@ -66,7 +153,23 @@ public class BowlerConfigPanel extends JPanel{
 		this.printer = printer;
 		
 	}
-
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		if (!Character.isDigit(arg0.getKeyChar())){
+			arg0.consume();
+		}
+	}
 	
 
 	
