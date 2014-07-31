9750d6b20e4f2f1264683cc4824bd7bd351518b4
DrVulpinus <siganus.vulpinus@live.com>
2014-07-31 15:46:11 -0400

    G-Code Viewer Improvements
    Improvements:
    -Everything now works
    -Non-extrusion moves are now displayed as lines
    -Moves with extrusion are displayed as filled boxes
    -Extrusion lines now have a height based on layers and width determined
    by the volume of the extrusion
    -Moves will change color if a potential fault is detected in them

--- application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
@@ -27,6 +27,7 @@ import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.VertexBuffer;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Line;
 import com.jme3.system.AppSettings;
@@ -39,7 +40,10 @@ public class MachineSimDisplay extends SimpleApplication{
 	JPanel panel;
 	private ArrayList<Geometry> shapes = new ArrayList<Geometry>();
 	private boolean hasChanged = false;
+	GCodes codes;
 	private ChaseCamera chaseCam;
+	Node obj;
+	Matrix3f rotateUp;
 	public MachineSimDisplay(JPanel _panel){
 		
 		panel = _panel;
@@ -47,49 +51,126 @@ public class MachineSimDisplay extends SimpleApplication{
 	
 	public void loadGCode(GCodes _codes){
 		shapes.clear();
+		codes = _codes;
 		for (int i = 1; i < _codes.size(); i++) {
 			GCodePosition code = _codes.get(i);
-			GCodePosition prevCode = _codes.get(i-1);
-			lineBuilder(prevCode.getX(), code.getX(),
-						prevCode.getY(), code.getY(),
-						prevCode.getZ(), code.getZ(),
-						_codes.getLayerHeight(code));
+			if (_codes.isPrintMove(code)){
+				boxBuilder(code);
+			}
+			else{
+				lineBuilder(code);
+			}
+			
 		}
+		System.out.println("Num of Codes: " + _codes.size());
+		System.out.println("Num of Shapes: " + shapes.size());
 		hasChanged = true;
 	}
 	
-	public void lineBuilder(double x1, double x2, double y1, double y2, double z1, double z2, double layerHeight){
+	public void boxBuilder(GCodePosition _code){
+		GCodePosition prevCode =codes.getPrevCode(_code);
+		if (prevCode != null){
+			
+			double x1 =  prevCode.getX();
+			double y1 =  prevCode.getY();
+			double z1 =  prevCode.getZ();
+			
+			double x2 =  _code.getX();
+			double y2 =  _code.getY();
+			double z2 =  _code.getZ();
+			if ((x1 > 200) || (y1 > 200) || (z1 > 200)){
+				System.out.println("The Ones: (" + x1 + ","+ y1 + "," + z1 + ")");
+			}
+			if ((x2 > 200) || (y2 > 200) || (z2 > 200)){
+				System.out.println("The Twos: (" + x2 + ","+ y2 + "," + z2 + ")");
+			}
+			
 		float centerX = (float) ((x1 + x2)/2);
 		float centerY = (float) ((y1 + y2)/2);
-		float centerZ = (float) ((z1 + z2)/2);
-		float extentX = (float) (Math.sqrt((Math.pow((x2-x1),2))+(Math.pow((y2-y1),2))+(Math.pow((z2-z1),2))));
-		float extentY = (float) (0.5);
-		float extentZ = (float) (layerHeight/2);
-		Vector3f center = new Vector3f(centerX, centerY, centerZ);
-		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1).normalize();
-		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2).normalize();
-		Vector3f dir = end.subtract(start).normalize();
-		Vector3f init = new Vector3f(1,1,1);
+		float centerZ = (float) ((z1 +z2)/2);
+		
+		float extentX = (float) codes.getMoveLength(_code);
+		
+		extentX = extentX/2;
+		float extentY = (float) ((codes.getMoveVolExtWidth(_code))/2);
+		float extentZ = (float) ((codes.getLayerHeight(_code))/2);
+	
+		
+		
+		Vector3f zeroCenter = new Vector3f(0,0,0);
+		
+		Vector3f newCenter = new Vector3f(centerX, centerY, centerZ);
+		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
+		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
+		Vector3f dir = end.subtract(start).normalizeLocal();
+		
+		
+		Vector3f init = new Vector3f(1,0,0);
 		Matrix3f direction = new Matrix3f();
-		direction.fromStartEndVectors(init, dir);
-		Quaternion quat = new Quaternion();
-		quat.fromRotationMatrix(direction);
-		Line l = new Line(start, end);
-		
-		Box b = new Box(extentX, extentY, extentZ);
-		
-       //b.updateGeometry(center, extentX, extentY, extentZ);
-        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
-        geom.getLocalRotation().lookAt(dir, new Vector3f(0,1,0));
-        //geom.setLocalRotation( direction);
-        geom.move(center);
-        //geom.setLocalTranslation(center);
+		direction.fromStartEndVectors(init, dir);		
+		Box b = new Box();
+		b.updateGeometry(zeroCenter, extentX, extentY, extentZ);
+       
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape      
+        
+        geom.setLocalRotation(direction);
+        geom.setLocalTranslation(newCenter);
+        
         Material mat = new Material(assetManager,
-          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material        
-        mat.setColor("Color", ColorRGBA.Blue);
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material        \
+        
+        if (codes.isGoodExtrusion(_code)){
+        	mat.setColor("Color", ColorRGBA.Green);
+        }
+        else{
+        	mat.setColor("Color", ColorRGBA.Red);
+        }
+        if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
+		//	System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
+			mat.setColor("Color", ColorRGBA.Yellow);
+		}
         geom.setMaterial(mat);                   // set the cube's material
         shapes.add(geom);
-       // rootNode.attachChild(geom);
+		}
+       
+	}
+	
+	public void lineBuilder(GCodePosition _code){
+		GCodePosition prevCode =codes.getPrevCode(_code);
+		if (prevCode != null){
+			
+			double x1 =  prevCode.getX();
+			double y1 =  prevCode.getY();
+			double z1 =  prevCode.getZ();
+			
+			double x2 =  _code.getX();
+			double y2 =  _code.getY();
+			double z2 =  _code.getZ();
+			
+			
+		
+		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
+		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
+		
+       Line l = new Line(start, end);
+        Geometry geom = new Geometry("Line", l);  // create line geometry from the shape      
+        
+        
+        
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material        \
+        
+       mat.setColor("Color", ColorRGBA.Blue);
+       
+       /* if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
+			System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
+			mat.setColor("Color", ColorRGBA.Green);
+		}
+		*/
+        geom.setMaterial(mat);                   // set the cube's material
+        shapes.add(geom);
+		}
+       
 	}
 	
 	@Override
@@ -127,7 +208,7 @@ public class MachineSimDisplay extends SimpleApplication{
        
         
         
-        
+       
         chaseCam = new ChaseCamera(getCamera(),rootNode, getInputManager());
         chaseCam.setDefaultDistance(200);
         chaseCam.setMaxDistance(100000);
@@ -137,9 +218,11 @@ public class MachineSimDisplay extends SimpleApplication{
         chaseCam.setMaxVerticalRotation((float) Math.PI);
         chaseCam.setMinVerticalRotation(((float) Math.PI)*-1);
         chaseCam.setInvertVerticalAxis(true);
-        chaseCam.setDefaultHorizontalRotation((float) ((Math.PI/2)));
-        chaseCam.setDefaultVerticalRotation((float) (-1*(Math.PI/4)));
-        //chaseCam.setUpVector(new Vector3f(0,0,1));
+        
+        //chaseCam.setDefaultHorizontalRotation((float) ((Math.PI/2)));
+        //chaseCam.setDefaultVerticalRotation((float) (-1*(Math.PI/4)));
+        //chaseCam.setUpVector(new Vector3f(0,0,-1));
+        
        // chaseCam.setDragToRotate(true);
         setPauseOnLostFocus(false);
         Node nodeLight = new Node();
@@ -150,6 +233,13 @@ public class MachineSimDisplay extends SimpleApplication{
         sun.setColor(ColorRGBA.White);
         nodeLight.addLight(sun);
         
+        obj = new Node();
+        Vector3f init = new Vector3f(0,1,0);
+        Vector3f end = new Vector3f(0,0,1);
+        rotateUp = new Matrix3f();
+        rotateUp.fromStartEndVectors(init, end);
+        //obj.setLocalRotation(rotateUp);
+        rootNode.attachChild(obj);
         //getFlyByCamera().setDragToRotate(true);
         
         
@@ -160,13 +250,13 @@ public class MachineSimDisplay extends SimpleApplication{
 		if (hasChanged == true){
 			hasChanged = false;
 			System.out.println("Number of lines: " + shapes.size());
-			rootNode.detachAllChildren();
+			obj.detachAllChildren();
 			for (int i = 0; i < shapes.size(); i++) {
-				rootNode.attachChild(shapes.get(i));
+				obj.attachChild(shapes.get(i));
 			}
 			System.out.println("How many children: " + rootNode.getChildren().size());
 		}
-		//System.out.println("Vertical: " + chaseCam.getVerticalRotation() + "Horizontal: " + chaseCam.getHorizontalRotation());
+		System.out.println("Vertical: " + chaseCam.getVerticalRotation() + "Horizontal: " + chaseCam.getHorizontalRotation());
 	 }
 	
 	protected class SwingApplicationStarter implements Runnable {
