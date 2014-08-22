592e20cf5a8b3bd82d7e8aafae5f399a5ee82a2e
DrVulpinus <siganus.vulpinus@live.com>
2014-08-22 13:06:05 -0400

    Complete Redesign of Geometry Generation
    Prior to this setup many issues were experienced with the loading and
    display off G-Code files.  Large files would crash the loading, and
    batching made excluding layers very difficult.
    
    New method creates geometries and batches them per layer to prevent
    memory overruns.
    
    Layers are then batched together when layer display changes are made.

--- application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.text.DefaultEditorKit.CutAction;
 
 import jme3test.games.CubeField;
 import jme3tools.optimize.GeometryBatchFactory;
@@ -35,6 +36,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.GeometryList;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
@@ -53,9 +55,8 @@ public class MachineSimDisplay extends SimpleApplication{
 	JmeCanvasContext ctx;
 	JPanel panel;
 	private ArrayList<Geometry> shapes = new ArrayList<Geometry>();
-	private ArrayList<Geometry> shapesCombined = new ArrayList<Geometry>();
 	private boolean hasChanged = false;
-	GCodes codes;
+	
 	private ChaseCamera chaseCam;
 	Node obj;
 	Matrix3f rotateUp;
@@ -68,24 +69,29 @@ public class MachineSimDisplay extends SimpleApplication{
 	private boolean loading= false;
 	private BitmapText loadingText = null;
 	private Vector3f scaleRate = new Vector3f(.5f,.5f,.5f);
-	private Vector3f printVolume = new Vector3f(200,200,200);
-	private Mesh pVol;
-	private JobExecPanel jep;
-	private BoundingBox pbb;
+	
+	
 	private boolean showGood = true;
 	private boolean showTroubled = true;
 	private boolean showDangerous = true;
 	private boolean showNonExtrude = true;
+	private PrintObject printObj;
 	private List<PrintTestListener> listeners = new ArrayList<PrintTestListener>();
 	public MachineSimDisplay(JPanel _panel){		
 		panel = _panel;
-		
+		printObj = new PrintObject(this);
 	}
 	
 	public void configure(float _printX, float _printY, float _printZ){
-		printVolume.set(_printX, _printY, _printZ);
+		printObj.configure(_printX, _printY, _printZ);
+		
 	}
-	
+	public void configureRect(float _printX, float _printY, float _printZ){
+		printObj.configureRect(_printX, _printY, _printZ);
+	}
+	public void configureCylinder(float _printR, float _printZ){
+		printObj.configureCylinder(_printR, _printZ);
+	}
 	public void addListener(PrintTestListener toAdd) {
         listeners.add(toAdd);
     }
@@ -94,7 +100,7 @@ public class MachineSimDisplay extends SimpleApplication{
 	}
 	
 	public boolean isPrintAllowed(){
-		for (Geometry geo : shapesCombined) {
+		for (Geometry geo : shapes) {
 			if (geo.getMaterial() == getMatFail()){
 				return false;
 			}
@@ -102,67 +108,20 @@ public class MachineSimDisplay extends SimpleApplication{
 		return true;
 	}
 	public boolean isPrintWarned(){
-		for (Geometry geo : shapesCombined) {
+		for (Geometry geo : shapes) {
 			if (geo.getMaterial() == getMatProblem()){
 				return true;
 			}
 		}
 		return false;
 	}
-	private Mesh getPrintVol(){
-		if (pVol == null){
-		
-		if (printVolume.getY()==0){//It's a cylinder
-			pVol = new Cylinder(100,100,(printVolume.getX()/2),printVolume.getZ(), true, false);
-		}
-		else{//It's a cube
-			Box b = new Box();
-			Vector3f printCenter = new Vector3f(printVolume.getX()/2,printVolume.getY()/2,0);//TODO: This should be made a member variable for adjustable print centers
-			b.updateGeometry(printCenter, printVolume.getX()/2, printVolume.getY()/2, printVolume.getZ()/2);
-			pVol = b;
-		}
-		}
-		
-		return pVol;
-		
-	}
-	private boolean isCubeVol(){
-		if (printVolume.getY() == 0){
-			return false;
-		}
-		else{
-			return true;
-		}
-	}
-	private Mesh getPrintBase(){
-		Mesh base;
-		
-		if (printVolume.getY()==0){//It's a cylinder
-			base = new Cylinder(100,100,(printVolume.getX()/2),0,true,false);
-		}
-		else{//It's a cube
-			Box b = new Box();
-			Vector3f printCenter = new Vector3f(printVolume.getX()/2,printVolume.getY()/2,0);//TODO: This should be made a member variable for adjustable print centers
-			b.updateGeometry(printCenter, printVolume.getX()/2, printVolume.getY()/2, 0);
-			base = b;
-		}
-		
-		
-		return base;
-		
-	}
-	private BoundingBox getVolBB(){
-		if (pbb == null){
-			pbb = new BoundingBox((BoundingBox) getPrintVol().getBound());
-		}
-		
-		
-		return pbb;
-	}
+	
+	
+
 	
 	
 	
-	private Material getMatGood(){
+	public Material getMatGood(){
 		if (matGood == null){
 			matGood = new Material(assetManager,  // Create new material and...
 	        	    "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
@@ -173,7 +132,7 @@ public class MachineSimDisplay extends SimpleApplication{
 		return matGood;
 		
 	}
-	private Material getMatProblem(){
+	public Material getMatProblem(){
 		if (matBad == null){
 			matBad = new Material(assetManager,  // Create new material and...
 	        	    "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
@@ -183,7 +142,7 @@ public class MachineSimDisplay extends SimpleApplication{
 		}
 		return matBad;
 	}
-	private Material getMatLine(){
+	public Material getMatLine(){
 		if (matLine == null){
 			matLine = new Material(assetManager,
 			          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material        \
@@ -192,7 +151,7 @@ public class MachineSimDisplay extends SimpleApplication{
 		}
 		return matLine;
 	}
-	private Material getMatFail(){
+	public Material getMatFail(){
 		if (matFail == null){
 			matFail = new Material(assetManager,  // Create new material and...
 	        	    "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
@@ -208,195 +167,33 @@ public class MachineSimDisplay extends SimpleApplication{
 	}
 	public void setLayersToShow(int numLayers){
 		
-		layersToShow= numLayers;
-		for (int i = 0; i < shapes.size(); i++) {
-			Geometry shape = shapes.get(i);
-			
-			if (codes.getLayer(codes.get(i)) > layersToShow){
-				shape.setCullHint(CullHint.Always);
-			}
-			else{
-				shape.setCullHint(CullHint.Inherit);
-			}
-		}
-		shapesCombined = (ArrayList<Geometry>) GeometryBatchFactory.makeBatches(shapes);
-		
-		System.out.println("Items after Combination: " + shapesCombined.size());
-		
+		shapes.clear();
+		shapes = printObj.getBatchedLayers(numLayers);
+		layersToShow = numLayers;
 		hasChanged = true;
+		
 	}
 	
 	
 	public int loadGCode(GCodes _codes){
 		
+		
 		shapes.clear();
 		
-		codes = _codes;
 		
-		for (GCodePosition code : _codes) {
-			if (_codes.isPrintMove(code)){
-				boxBuilder(code);
-			}
-			else{
-				lineBuilder(code);
-			}
-			
-		}
-		
+		printObj = new PrintObject(_codes, this);
+		printObj.processGCodes();
+		shapes = printObj.getBatchedObject();
 		System.out.println("Num of Codes: " + _codes.size());
 		System.out.println("Num of Shapes: " + shapes.size());
 		lastShownIndex = shapes.size();
-		shapesCombined = (ArrayList<Geometry>) GeometryBatchFactory.makeBatches(shapes);
-		System.out.println("Items after Combination: " + shapesCombined.size());
+		
 		hasChanged = true;
-		return codes.numLayers();
+		return printObj.getNumLayers();
 	}
 	
-	public void boxBuilder(GCodePosition _code){
-		GCodePosition prevCode =codes.getPrevCode(_code);
-		if (prevCode != null){
-			
-			float extentX = (float) codes.getMoveLength(_code);
-			
-			extentX = extentX/2;
-			float extentY = (float) ((codes.getMoveVolExtWidth(_code))/2);
-			float extentZ = (float) ((codes.getLayerHeight(_code))/2);
-			if (!Float.isFinite(extentX) || !Float.isFinite(extentY) || !Float.isFinite(extentZ)){
-				return;
-			}
-			
-			double x1 =  prevCode.getX();
-			double y1 =  prevCode.getY();
-			double z1 =  prevCode.getZ();
-			
-			double x2 =  _code.getX();
-			double y2 =  _code.getY();
-			double z2 =  _code.getZ();
-			if ((x1 > 200) || (y1 > 200) || (z1 > 200)){
-				System.out.println("The Ones: (" + x1 + ","+ y1 + "," + z1 + ")");
-			}
-			if ((x2 > 200) || (y2 > 200) || (z2 > 200)){
-				System.out.println("The Twos: (" + x2 + ","+ y2 + "," + z2 + ")");
-			}
-			
-		float centerX = (float) ((x1 + x2)/2);
-		float centerY = (float) ((y1 + y2)/2);
-		float centerZ = (float) ((z1 +z2)/2);
-		
-		
 	
-		
-		
-		Vector3f zeroCenter = new Vector3f(0,0,0);
-		
-		Vector3f newCenter = new Vector3f(centerX, centerY, centerZ);
-		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
-		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
-		Vector3f dir = end.subtract(start).normalizeLocal();
-		
-		
-		Vector3f init = new Vector3f(1,0,0);
-		Matrix3f direction = new Matrix3f();
-		direction.fromStartEndVectors(init, dir);		
-		Box b = new Box();
-		b.updateGeometry(zeroCenter, extentX, extentY, extentZ);
-       
-        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape      
-        
-        geom.setLocalRotation(direction);
-        geom.setLocalTranslation(newCenter);
-        
-        
-        	
-        	            
-        
-        if (codes.isGoodExtrusion(_code)){
-        	geom.setMaterial(getMatGood());
-        	geom.setName("Good Extrude");
-        }
-        else{
-        	geom.setMaterial(getMatProblem());
-        	geom.setName("Problem Extrude");
-        }
-        if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
-		//	System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
-        	geom.setMaterial(getMatProblem());
-        	geom.setName("Problem Extrude");
-		}
-               
-        /*TODO: this is bad...
-         * we should be able to find a way to check for this regardless of build volume shape
-         */
-        if (isCubeVol()){
-        	if (!getVolBB().contains(end) || !getVolBB().contains(start)){
-        		geom.setMaterial(getMatFail());
-        		geom.setName("Fail Extrude");
-        	}
-        }
-        else{
-        	Vector3f layerCenterStart = new Vector3f(getVolBB().getCenter().getX(), getVolBB().getCenter().getY(), start.getZ());
-        	Vector3f layerCenterEnd = new Vector3f(getVolBB().getCenter().getX(), getVolBB().getCenter().getY(), end.getZ());
-        	if (layerCenterEnd.distance(end) > (printVolume.getX()/2)){
-        		geom.setMaterial(getMatFail());
-        		geom.setName("Fail Extrude");
-        	}
-        	if (layerCenterStart.distance(start) > (printVolume.getX()/2)){
-        		geom.setMaterial(getMatFail());
-        		geom.setName("Fail Extrude");
-        	}
-        }
-        
-        	  
-        	  // set the cube's material
-        
-        
-			shapes.add(geom);
-		
-        
-        //hasChanged = true;
-		}
-       
-	}
 	
-	public void lineBuilder(GCodePosition _code){
-		GCodePosition prevCode =codes.getPrevCode(_code);
-		if (prevCode != null){
-			
-			double x1 =  prevCode.getX();
-			double y1 =  prevCode.getY();
-			double z1 =  prevCode.getZ();
-			
-			double x2 =  _code.getX();
-			double y2 =  _code.getY();
-			double z2 =  _code.getZ();
-			
-			
-		
-		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
-		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
-		
-       Line l = new Line(start, end);
-        Geometry geom = new Geometry("Line", l);  // create line geometry from the shape      
-        
-        
-        
-        
-       
-       /* if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
-			System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
-			mat.setColor("Color", ColorRGBA.Green);
-		}
-		*/
-        geom.setMaterial(getMatLine());                   // set the cube's material
-        try {
-			shapes.set(codes.indexOf(_code), geom);
-		} catch (Exception e) {
-			shapes.add(geom);
-		}
-        //hasChanged = true;
-		}
-       
-	}
 	
 	@Override
     public void start() {
@@ -430,7 +227,7 @@ public class MachineSimDisplay extends SimpleApplication{
 		    
         flyCam.setEnabled(false);
         
-       BoundingBox bb = getVolBB();
+       BoundingBox bb = printObj.getVolBB();
         
         
         chaseCam = new ChaseCamera(getCamera(),rootNode, getInputManager());
@@ -490,7 +287,7 @@ public class MachineSimDisplay extends SimpleApplication{
         
         //Quad base = new Quad(200, 200);
         
-        Geometry base = new Geometry("Base", getPrintBase());
+        Geometry base = new Geometry("Base", printObj.getPrintBase());
         //geom.setLocalTranslation(new Vector3f(-250,-250,0));
         Material mat = new Material(assetManager,  // Create new material and...
         	    "Common/MatDefs/Light/Lighting.j3md"); // ... specify .j3md file to use (illuminated).
@@ -595,7 +392,7 @@ public class MachineSimDisplay extends SimpleApplication{
 	
 	
 	@Override
-	 public void simpleUpdate(float tpf){
+	public void simpleUpdate(float tpf){
 		if (loading == true  && hasChanged == false){
 			//Do something to let the user know things are happening			
 	        guiNode.attachChild(getLoadingText(tpf));
@@ -614,7 +411,7 @@ public class MachineSimDisplay extends SimpleApplication{
 			
 			obj.detachAllChildren();
 			
-			for (Geometry geom : shapesCombined) {
+			for (Geometry geom : shapes) {
 				if (geom.getMaterial() == getMatGood() && isShowGood()){
 					obj.attachChild(geom);
 				}
---
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/PrintObject.java
@@ -0,0 +1,335 @@
+package com.neuronrobotics.nrconsole.plugin.JobExec;
+
+import java.util.ArrayList;
+
+import jme3tools.optimize.GeometryBatchFactory;
+
+import com.jme3.bounding.BoundingBox;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Cylinder;
+import com.jme3.scene.shape.Line;
+
+public class PrintObject{
+	
+	
+	private ArrayList<LayerGeoms> layers = new ArrayList<LayerGeoms>();
+	private GCodes codes;
+	private MachineSimDisplay msd;
+
+
+	private Vector3f printVolume = new Vector3f(200,0,200);
+	private Mesh pVol;
+	private BoundingBox pbb;
+	
+	
+	public PrintObject ( MachineSimDisplay _msd){
+		codes = null;
+		msd = _msd;
+	}
+	public PrintObject (GCodes _codes, MachineSimDisplay _msd){
+		codes = _codes;
+		msd = _msd;
+	}
+	
+	public void configure(float _printX, float _printY, float _printZ){
+		printVolume.set(_printX, _printY, _printZ);
+		
+	}
+	public void configureRect(float _printX, float _printY, float _printZ){
+		printVolume.set(_printX, _printY, _printZ);
+	}
+	public void configureCylinder(float _printR, float _printZ){
+		printVolume.set(_printR, 0, _printZ);
+	}
+	
+	
+	
+	public Mesh getPrintVol(){
+		if (pVol == null){
+		
+		if (printVolume.getY()==0){//It's a cylinder
+			pVol = new Cylinder(100,100,(printVolume.getX()/2),printVolume.getZ(), true, false);
+		}
+		else{//It's a cube
+			Box b = new Box();
+			Vector3f printCenter = new Vector3f(printVolume.getX()/2,printVolume.getY()/2,0);//TODO: This should be made a member variable for adjustable print centers
+			b.updateGeometry(printCenter, printVolume.getX()/2, printVolume.getY()/2, printVolume.getZ()/2);
+			pVol = b;
+		}
+		}
+		
+		return pVol;
+		
+	}
+	public boolean isCubeVol(){
+		if (printVolume.getY() == 0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	public Mesh getPrintBase(){
+		Mesh base;
+		
+		if (printVolume.getY()==0){//It's a cylinder
+			base = new Cylinder(100,100,(printVolume.getX()/2),0,true,false);
+		}
+		else{//It's a cube
+			Box b = new Box();
+			Vector3f printCenter = new Vector3f(printVolume.getX()/2,printVolume.getY()/2,0);//TODO: This should be made a member variable for adjustable print centers
+			b.updateGeometry(printCenter, printVolume.getX()/2, printVolume.getY()/2, 0);
+			base = b;
+		}
+		
+		
+		return base;
+		
+	}
+	public BoundingBox getVolBB(){
+		if (pbb == null){
+			pbb = new BoundingBox((BoundingBox) getPrintVol().getBound());
+		}
+		
+		
+		return pbb;
+	}
+	
+	
+	private Geometry buildGeom(GCodePosition _code){
+		if (codes.isPrintMove(_code)){
+			return boxBuilder(_code);
+		}
+		else{
+			return lineBuilder(_code);
+		}
+	}
+	private Geometry boxBuilder(GCodePosition _code){
+		//prevTime = System.currentTimeMillis();
+		GCodePosition prevCode =codes.getPrevCode(_code);
+		if (prevCode != null){
+			
+			float extentX = (float) codes.getMoveLength(_code);
+			
+			extentX = extentX/2;
+			float extentY = (float) ((codes.getMoveVolExtWidth(_code))/2);
+			float extentZ = (float) ((codes.getLayerHeight(_code))/2);
+			if (!Float.isFinite(extentX) || !Float.isFinite(extentY) || !Float.isFinite(extentZ)){
+				return null;
+			}
+			
+			double x1 =  prevCode.getX();
+			double y1 =  prevCode.getY();
+			double z1 =  prevCode.getZ();
+			
+			double x2 =  _code.getX();
+			double y2 =  _code.getY();
+			double z2 =  _code.getZ();
+			if ((x1 > 200) || (y1 > 200) || (z1 > 200)){
+				System.out.println("The Ones: (" + x1 + ","+ y1 + "," + z1 + ")");
+			}
+			if ((x2 > 200) || (y2 > 200) || (z2 > 200)){
+				System.out.println("The Twos: (" + x2 + ","+ y2 + "," + z2 + ")");
+			}
+			
+		float centerX = (float) ((x1 + x2)/2);
+		float centerY = (float) ((y1 + y2)/2);
+		float centerZ = (float) ((z1 +z2)/2);
+		
+		
+	
+		
+		
+		Vector3f zeroCenter = new Vector3f(0,0,0);
+		
+		Vector3f newCenter = new Vector3f(centerX, centerY, centerZ);
+		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
+		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
+		Vector3f dir = end.subtract(start).normalizeLocal();
+		
+		
+		Vector3f init = new Vector3f(1,0,0);
+		Matrix3f direction = new Matrix3f();
+		direction.fromStartEndVectors(init, dir);		
+		Box b = new Box();
+		b.updateGeometry(zeroCenter, extentX, extentY, extentZ);
+       
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape      
+        
+        geom.setLocalRotation(direction);
+        geom.setLocalTranslation(newCenter);
+        
+        
+        	
+        	            
+        
+        if (codes.isGoodExtrusion(_code)){
+        	geom.setMaterial(msd.getMatGood());
+        	geom.setName("Good Extrude");
+        }
+        else{
+        	geom.setMaterial(msd.getMatProblem());
+        	geom.setName("Problem Extrude");
+        }
+        if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
+		//	System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
+        	geom.setMaterial(msd.getMatProblem());
+        	geom.setName("Problem Extrude");
+		}
+               
+        /*TODO: this is bad...
+         * we should be able to find a way to check for this regardless of build volume shape
+         */
+        if (isCubeVol()){
+        	if (!getVolBB().contains(end) || !getVolBB().contains(start)){
+        		geom.setMaterial(msd.getMatFail());
+        		geom.setName("Fail Extrude");
+        	}
+        }
+        else{
+        	Vector3f layerCenterStart = new Vector3f(getVolBB().getCenter().getX(), getVolBB().getCenter().getY(), start.getZ());
+        	Vector3f layerCenterEnd = new Vector3f(getVolBB().getCenter().getX(), getVolBB().getCenter().getY(), end.getZ());
+        	if (layerCenterEnd.distance(end) > (printVolume.getX()/2)){
+        		geom.setMaterial(msd.getMatFail());
+        		geom.setName("Fail Extrude");
+        	}
+        	if (layerCenterStart.distance(start) > (printVolume.getX()/2)){
+        		geom.setMaterial(msd.getMatFail());
+        		geom.setName("Fail Extrude");
+        	}
+        }
+        
+        	  
+        	  // set the cube's material
+        return(geom);
+        
+			//shapes.add(geom);
+		
+        
+        //hasChanged = true;
+		}
+		return null;
+       
+	}
+	private Geometry lineBuilder(GCodePosition _code){
+		//prevTime = System.currentTimeMillis();
+		GCodePosition prevCode =codes.getPrevCode(_code);
+		if (prevCode != null){
+			
+			double x1 =  prevCode.getX();
+			double y1 =  prevCode.getY();
+			double z1 =  prevCode.getZ();
+			
+			double x2 =  _code.getX();
+			double y2 =  _code.getY();
+			double z2 =  _code.getZ();
+			
+			
+		
+		Vector3f start = new Vector3f((float)x1,(float)y1,(float)z1);
+		Vector3f end = new Vector3f((float)x2,(float)y2,(float)z2);
+		
+       Line l = new Line(start, end);
+        Geometry geom = new Geometry("Line", l);  // create line geometry from the shape      
+        
+        
+        
+        
+       
+       /* if ((extentX > 100) || (extentY > 1) || (extentZ > 1)){
+			System.out.println("The Extents: (" + x2 + ","+ y2 + "," + z2 + ")");
+			mat.setColor("Color", ColorRGBA.Green);
+		}
+		*/
+        geom.setMaterial(msd.getMatLine());                   // set the cube's material
+        return geom;
+		}
+		return null;
+	}
+	
+	
+	
+	
+	public int getNumLayers(){
+		return layers.size();
+	}
+	
+	/**
+	 * This method batches all of the raw geometries in a layer
+	 * @param _layer The layer in which to batch all geometries
+	 */
+	private void batchLayer(int _layer){
+		if (_layer < getNumLayers()){
+			ArrayList<Geometry> x =  (ArrayList<Geometry>) GeometryBatchFactory.makeBatches(layers.get(_layer));
+			layers.get(_layer).clear();
+			layers.get(_layer).addAll(x);
+		}
+	}
+	
+	/**
+	 * This method batches the geometries in all of the layers
+	 */
+	private void batchAllLayers(){
+		for (LayerGeoms layer : layers) {
+			ArrayList<Geometry> x =  (ArrayList<Geometry>) GeometryBatchFactory.makeBatches(layer);
+			layer.clear();
+			layer.addAll(x);
+		}
+	}
+	
+/**
+ * After all GCodes have be loaded, process them to create geometries representing each one
+ */
+	public void processGCodes(){
+		int layer = 0;
+		layers.clear();
+		layers.add(new LayerGeoms());
+		System.out.println("Begin Processing GCodes!");
+		for (GCodePosition code : codes) {
+			if (codes.getLayer(code) != layer){
+				layers.add(new LayerGeoms());
+				batchLayer(layer);
+				layer++;
+				System.out.println("New Layer: " + layer);
+			}
+			Geometry geom = buildGeom(code);
+			if (geom != null){
+				layers.get(getNumLayers()-1).add(geom);
+			}
+		}
+		batchAllLayers();
+	}
+	
+	
+	/**
+	 * This method returns an ArrayList of Geometry (one for each material) which contains the batched 
+	 * geometries of all layers up to _upToLayer.
+	 * @param _upToLayer The number of the first layer which should not be displayed
+	 * @return The ArrayList of Geometry which represents the object up to the layer specified
+	 */
+	public ArrayList<Geometry> getBatchedLayers(int _upToLayer){
+		ArrayList<Geometry> longList = new ArrayList<Geometry>();
+		for (int i = 0; i < _upToLayer; i++) {
+			for (Geometry subGeom : layers.get(i)) {
+				longList.add(subGeom);
+			}
+		}		
+		return (ArrayList<Geometry>) GeometryBatchFactory.makeBatches(longList);
+		
+	}
+	
+	/**
+	 This method returns an ArrayList of Geometry (one for each material) which contains the batched 
+	 * geometries of the entire object. 
+	 * @return The ArrayList of Geometry which represents the object
+	 */
+	public ArrayList<Geometry> getBatchedObject(){
+		return getBatchedLayers(getNumLayers());
+	}
+	
+	
+}
