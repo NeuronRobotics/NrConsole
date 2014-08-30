25db4eb204478349e77492b4078a8cdfbd0933e4
Paula Rudy <paularudy@gmail.com>
2014-08-30 18:03:36 -0400

    Minor clean up. Some formatting changes, removal of commented out code.

--- application/nrconsole/src/com/neuronrobotics/nrconsole/NRConsoleWindow.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/NRConsoleWindow.java
@@ -1,11 +1,9 @@
 package com.neuronrobotics.nrconsole;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Rectangle;
 import java.awt.event.HierarchyBoundsListener;
 import java.awt.event.HierarchyEvent;
 import java.util.ArrayList;
@@ -17,44 +15,40 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.Scrollable;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
-import net.miginfocom.swing.MigLayout;
 
 import com.neuronrobotics.nrconsole.plugin.IPluginUpdateListener;
 import com.neuronrobotics.nrconsole.plugin.PluginManager;
 import com.neuronrobotics.sdk.config.SDKBuildInfo;
 import com.neuronrobotics.sdk.ui.ConnectionImageIconFactory;
 
-import java.awt.BorderLayout;
-
 
 public class NRConsoleWindow extends JFrame implements IPluginUpdateListener {
 	private ArrayList<JPanel> panels=new ArrayList<JPanel>();
 	private static final String name = "Neuron Robotics Console ";
-//	public static int panelHight = 700;
-//	public static int panelWidth = 1095;
+
 	private static final long serialVersionUID = 1L;
 	private JPanel scroller = new  JPanel();
 	private JScrollPane scrollPanel = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 	
 	private JTabbedPane modePane = new JTabbedPane();
-	private JPanel active=null;
+
 	private static NRConsoleWindow instance = null;
 	public ImageIcon logo = new ImageIcon(NRConsole.class.getResource("images/logo.png"));
 	private PluginManager manager;
+	
 	public static Dimension getNRWindowSize(){
 		if(instance!= null)
 			return instance.getWindowSize();
 		return  new Dimension(400,400);
 	}
+	
 	public Dimension getWindowSize(){
 		Dimension d = new Dimension(getWidth(),getHeight());
-		//System.out.println("Window size "+d);
 		return d;
 	}
+	
 	private JPanel logoPanel = new JPanel();
 	
 	public NRConsoleWindow() {
