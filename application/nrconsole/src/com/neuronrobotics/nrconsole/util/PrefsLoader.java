1ecfc7ef373e24432f99ff5cda7bd1da54ac2fd0
DrVulpinus <siganus.vulpinus@live.com>
2014-09-01 18:30:52 -0400

    Added Class for saving local preferences
    Preferences can now be easily saved and loaded for user specific options

---
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/util/PrefsLoader.java
@@ -0,0 +1,17 @@
+package com.neuronrobotics.nrconsole.util;
+
+import java.util.prefs.Preferences;
+
+public class PrefsLoader {
+	private static final String SLIC3R_LOCATION = "slic3r_path";
+	static String path = "/usr/local/Slic3r/bin/slic3r";
+	Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+	
+	
+	public String getSlic3rLocation(){
+		return prefs.get(SLIC3R_LOCATION, path);
+	}
+	public void setSlic3rLocation(String _path){
+		prefs.put(SLIC3R_LOCATION, _path);
+	}
+}
