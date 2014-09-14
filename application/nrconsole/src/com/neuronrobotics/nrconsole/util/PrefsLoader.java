ff8e9ea696f49861d1918fba07e2f48283dbb34e
DrVulpinus <siganus.vulpinus@live.com>
2014-09-13 23:23:37 -0400

    Added Method to reset to defaults

--- application/nrconsole/src/com/neuronrobotics/nrconsole/util/PrefsLoader.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/util/PrefsLoader.java
@@ -2,6 +2,8 @@ package com.neuronrobotics.nrconsole.util;
 
 import java.util.prefs.Preferences;
 
+import com.neuronrobotics.replicator.driver.Slic3r;
+
 public class PrefsLoader {
 	private static final String SLIC3R_LOCATION = "slic3r_path";
 	private static final String SLIC3R_RDBTN_LAST = "slic3r_rdbtn_last";
@@ -14,6 +16,7 @@ public class PrefsLoader {
 	}
 	public void setSlic3rLocation(String _path){
 		prefs.put(SLIC3R_LOCATION, _path);
+		Slic3r.setExecutableLocation(_path);
 	}
 	
 	public int getSlic3rRDBTNLast(){
@@ -23,5 +26,11 @@ public class PrefsLoader {
 		prefs.putInt(SLIC3R_RDBTN_LAST, _btn);
 	}
 	
+	
+	public void loadDefaults(){
+		setSlic3rLocation(path);
+		setSlic3rRDBTNLast(0);
+		Slic3r.setExecutableLocation(path);
+	}
 	
 }
