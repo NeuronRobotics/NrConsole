05fbefa736c47cdd16b4b9a772faa46329c0f82d
DrVulpinus <siganus.vulpinus@live.com>
2014-09-16 18:11:43 -0400

    Changed Loading of App Settings
    Hopefully this will correct errors on loading the application by
    allowing JME3 to use any available renderer

--- application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/plugin/JobExec/MachineSimDisplay.java
@@ -212,12 +212,17 @@ public class MachineSimDisplay extends SimpleApplication{
     public void start() {
         // set some default settings in-case
         // settings dialog is not shown
+		showSettings = true;
         boolean loadSettings = false;
-        if (settings == null) {
+        AppSettings test = new AppSettings(true);
+       
+       if (settings == null) {
             setSettings(new AppSettings(true));
             loadSettings = true;
         }
-
+       	settings.setRenderer(AppSettings.LWJGL_OPENGL_ANY);
+       	settings.setAudioRenderer(null);
+       	
         // show settings dialog
         /*if (showSettings) {
             if (!JmeSystem.showSettingsDialog(settings, loadSettings)) {
@@ -225,6 +230,7 @@ public class MachineSimDisplay extends SimpleApplication{
             }
         }*/
         //re-setting settings they can have been merged from the registry.
+       
         setSettings(settings);
         panel.setMinimumSize(new Dimension(100,100));
         createCanvas(); // create canvas!
