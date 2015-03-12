6277f3d58fbb364d983d0591efaee0cbb1cddfb7
madhephaestus <mad.hephaestus@gmail.com>
2015-03-12 00:11:34 -0400

    switcher the esquencer to wav files since mp3s cause the linux build to break. this should stabalize in later builds of java 8.

--- application/nrconsole/src/com/neuronrobotics/nrconsole/util/Mp3Filter.java
+++ application/nrconsole/src/com/neuronrobotics/nrconsole/util/Mp3Filter.java
@@ -7,17 +7,17 @@ import javax.swing.filechooser.FileFilter;
 public class Mp3Filter extends FileFilter {
 	
 	public String getDescription() {
-		return "MP3 Audio File (mp3)";
+		return "wav Audio File (wav)";
 	}
 	public boolean accept(File f) {
 		if(f.isDirectory()) {
 			return true;
 		}
 		String path = f.getAbsolutePath().toLowerCase();
-		if ((path.endsWith("mp3") && (path.charAt(path.length() - 3)) == '.')) {
+		if ( (path.endsWith("wav")&& (path.charAt(path.length() - 3)) == '.')) {
 			return true;
 		}
-		return f.getName().matches(".+\\.mp3$");
+		return f.getName().matches(".+\\.wav$");
 	}
 
 }
